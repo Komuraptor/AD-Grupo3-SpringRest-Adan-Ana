d926013da1eaa821b3d746e0b8744000655e1629
Ana <blasco.paana21@cadiz.salesianos.edu>
2023-02-26 22:13:17 +0100

    readme y sql

---
+++ SpringRestGrupo3-Productos/README.txt
@@ -0,0 +1,6 @@
+Usuario administrador:
+User: admin
+Password: admin
+
+Enlace a repositorio con frontend (debe estar en repositorio separado para funcionar):
+https://github.com/AnaBlascoParra/FrontAngularGrupo3
---
+++ SpringRestGrupo3-Productos/productos.sql
@@ -0,0 +1,168 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 16-02-2023 a las 09:42:30
+-- Versión del servidor: 10.4.25-MariaDB
+-- Versión de PHP: 8.1.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `productos`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `categoria`
+--
+
+CREATE TABLE `categoria` (
+  `id` int(11) NOT NULL,
+  `descripcion` varchar(255) DEFAULT NULL,
+  `nombre` varchar(255) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `categoria`
+--
+
+INSERT INTO `categoria` (`id`, `descripcion`, `nombre`) VALUES
+(2, 'Muebles como fregaderos, frigorificos, despensas, etc.', 'Cocina'),
+(3, 'Muebles como sofás, librerias, etc.', 'Salon'),
+(102, 'Muebles como retretes, bañeras, lavabos, etc.', 'Baño');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `categoria_seq`
+--
+
+CREATE TABLE `categoria_seq` (
+  `next_val` bigint(20) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `categoria_seq`
+--
+
+INSERT INTO `categoria_seq` (`next_val`) VALUES
+(201);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `producto`
+--
+
+CREATE TABLE `producto` (
+  `id` int(11) NOT NULL,
+  `descripcion` varchar(255) DEFAULT NULL,
+  `nombre` varchar(255) DEFAULT NULL,
+  `precio` int(11) NOT NULL,
+  `categoria_id` int(11) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `producto`
+--
+
+INSERT INTO `producto` (`id`, `descripcion`, `nombre`, `precio`, `categoria_id`) VALUES
+(5, 'Sofá de 4 plazas.', 'Sofá', 300, 3),
+(102, 'Bañera de 2 metros.', 'Bañera', 400, 102),
+(152, 'Horno con varios modos.', 'Horno', 300, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `producto_seq`
+--
+
+CREATE TABLE `producto_seq` (
+  `next_val` bigint(20) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `producto_seq`
+--
+
+INSERT INTO `producto_seq` (`next_val`) VALUES
+(251);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `user`
+--
+
+CREATE TABLE `user` (
+  `id` bigint(20) NOT NULL,
+  `enabled` bit(1) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `role` varchar(255) DEFAULT NULL,
+  `token` varchar(255) DEFAULT NULL,
+  `username` varchar(255) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `user`
+--
+
+INSERT INTO `user` (`id`, `enabled`, `password`, `role`, `token`, `username`) VALUES
+(1, b'1', '$2a$10$3kxKtOgn/.CzrEEz4fDT2u8qa0aRY30tox5KocHygaT3TMbJhr5dK', 'ROLE_ADMIN', NULL, 'admin');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `user_seq`
+--
+
+CREATE TABLE `user_seq` (
+  `next_val` bigint(20) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `user_seq`
+--
+
+INSERT INTO `user_seq` (`next_val`) VALUES
+(101);
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `categoria`
+--
+ALTER TABLE `categoria`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `producto`
+--
+ALTER TABLE `producto`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `FKodqr7965ok9rwquj1utiamt0m` (`categoria_id`);
+
+--
+-- Indices de la tabla `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `UK_sb8bbouer5wak8vyiiy4pf2bx` (`username`) USING HASH;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
